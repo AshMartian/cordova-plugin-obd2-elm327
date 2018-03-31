019a8ce4bbc9f7c72579476c2fd5050b1d1a482d
blandman <programmer.brandon@gmail.com>
2018-03-30 18:00:37 -0700

    Added bluetooth deps

--- src/android/obd2elm327.java
+++ src/android/obd2elm327.java
@@ -7,6 +7,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.util.Log;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.LOG;
+
+
+import java.util.Set;
+
 /**
  * This class echoes a string called from JavaScript.
  */
